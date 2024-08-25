1c9c76c8f08c52bc19e6c52be1b639ba9022423e
Living-Hell <s.living.hell@gmail.com>
2024-08-25 17:14:12 +0530

    Defining folder structure and User table structure

--- com.inn.cafe/src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/CafeManagementSystemApplication.java
@@ -2,8 +2,9 @@ package com.inn.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class CafeManagementSystemApplication {
 
 	public static void main(String[] args) {
